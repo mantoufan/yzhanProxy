ba63ef650e7dfa78001d9c2d5fb47a1b2528007e
馒头饭 <mhjlw@126.com>
2020-10-21 20:39:29 +0800

    :new:更新任务管理系统，采用session共享数据

--- mtfBetter.class.php
+++ mtfBetter.class.php
@@ -1,16 +1,22 @@
 <?php
 class mtfBetter
 {
-    public function handler($arv = array()) {        
-        $CONF = include 'conf.php';
+    public $CONF;
+    public function __construct($arv = array())
+    {
+        session_start();
+		$CONF = include dirname(__FILE__) . '/conf.php';
         forEach ($CONF['arv'] as $k => $v) {
             if (!empty($arv[$k])) {
                 $CONF['arv'][$k] = $arv[$k];
             }
         }
-
+        $this->CONF = $CONF;
+    }
+    
+    public function handler() {        
+        $CONF = $this->CONF;
         $_p = $CONF['arv']['path'];
-        
         if (file_exists($_p)) {
             $_i = pathinfo($_p);
             switch ($_i['extension']) {
@@ -19,7 +25,7 @@ class mtfBetter
                 case 'html':
                     if (isset($CONF['js'][$_i['basename']])) {
                         if (!empty($CONF['js'][$_i['basename']])) {
-                            header('Location: ' . CONF['js'][$_i['basename']]);
+                            header('Location: ' . $CONF['js'][$_i['basename']]);
                         }
                     } else {
                         $_p_cache = $CONF['arv']['cache_dir']. md5($_i['dirname'] . '/' . $_i['basename']) . '.' . $_i['extension'];
@@ -27,7 +33,7 @@ class mtfBetter
                             $_c = file_get_contents($_p_cache);
                             $this->cacheClear(10);
                         } else {
-                            $this->$taskManager(1, $_p, $_i['extension']);
+                            $this->taskManager(1, $_p);
                             include_once(dirname(__FILE__) . '/vendor/autoload.php');
                             $minifier = new \marcocesarato\minifier\Minifier();
                             $_c = file_get_contents($_p);
@@ -39,64 +45,67 @@ class mtfBetter
                                 $_c = $minifier->minifyHTML($_c);
                             }
                             file_put_contents($_p_cache, $_c);
-                            $this->$taskManager(0);
+                            $this->taskManager(0);
                         }
-                        contentType($_i['extension']);
+                        $this->contentType($_i['extension']);
                         die($_c);
                     }
                 break;
                 case 'jpeg':
                 case 'jpg':
-                    $this->webp(imagecreatefromstring(file_get_contents($_p)), md5($_i['basename']), 'jpeg');
-                    break;
                 case 'png':
-                    $image = imagecreatefromstring(file_get_contents($_p));
-                    imagepalettetotruecolor($image);
-                    imagealphablending($image, true);
-                    imagesavealpha($image, true);
-                    $this->webp($image, md5($_i['basename']), 'png');
-                    imagedestroy($image);
-                    break;
                 case 'gif':
-                    $this->webp(imagecreatefromstring(file_get_contents($_p)), md5($_i['basename']), 'gif');
-                    break;
+                    $this->taskManager(1, $_p);
+                    $_p = $this->webp($_p, md5($_i['basename']));
+                    $this->taskManager(0);
+                    if ($_p) {
+                        $this->contentType('webp');
+                    } else {
+                        $this->outPut($_p);
+                    }
                 default:
                     die(file_get_contents($_p));
             }
         }
     }
-    private function webp($image, $filename, $extension) {
-        if (!is_dir($CONF['arv']['cache_dir'])) {
-            mkdir($CONF['arv']['cache_dir']);
-        }
-        $this->cacheClear(5);
-        if(strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false) {
-            $this->$taskManager(1, $_p, $extension);
-            $_b = true;
-            $_p = $CONF['arv']['cache_dir']. $filename .'.webp';
-            if (!file_exists($_p)) {
-                try {
-                    imagepalettetotruecolor($image);
-                    $_b  = imagewebp($image, $_p, 75);
-                } catch (\Exception $e) {
-                    $_b  = false;
+    public function webp($_p, $filename) {
+        if (file_exists($_p)) {
+            $CONF = $this->CONF;
+            $image = imagecreatefromstring(file_get_contents($_p));
+            imagepalettetotruecolor($image);
+            imagealphablending($image, true);
+            imagesavealpha($image, true);
+    
+            if (!is_dir($CONF['arv']['cache_dir'])) {
+                mkdir($CONF['arv']['cache_dir']);
+            }
+            $this->cacheClear(5);
+            if(strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false) {
+                $_b = true;
+                $_p = $CONF['arv']['cache_dir']. $filename .'.webp';
+                if (!file_exists($_p)) {
+                    try {
+                        imagepalettetotruecolor($image);
+                        $_b  = imagewebp($image, $_p, 75);
+                    } catch (\Exception $e) {
+                        $_b  = false;
+                    }
+                }
+                imagedestroy($image);
+                if ($_b) {
+                    return $_p;
                 }
             }
-            $this->$taskManager(0);
-            if ($_b) {
-                contentType('webp');
-                die(file_get_contents($_p));
-            }
         }
-        contentType($extension);
-        if (!file_exists($_p)) {
-            $quality = $extension === 'png' ? 7 : 75;
-            $im = 'image' . $extension;
-            $im($image, $_p, $quality);
-        }
+        return false;
+    }
+    private function outPut($_p) {
+        $_i = pathinfo($_p);
+        $this->contentType($_i['extension']);
         die(file_get_contents($_p));
     }
     private function cacheClear($rand) {
+        $CONF = $this->CONF;
         if (rand(0, 100) > $rand) return;
         if (is_dir($CONF['arv']['cache_dir'])) {
             forEach(glob($CONF['arv']['cache_dir'] . '*.*') as $file) {
@@ -105,19 +114,6 @@ class mtfBetter
                 }
             }
         }
-    }
-    private function data($datas = null) {
-        $_p = $CONF['arv']['cache_dir'] . 'data.php';
-        if (file_exists($_p)) {
-            $data = include $_p;
-        } else {
-            $data = array();
-        }
-        if (isset($datas )) {
-            $data = array_merge($data, $datas);
-            file_put_contents($_p, '<?php return ' . var_export($data, true) . ';?>');
-        }  
-        return $data;
     }
     private function contentType($ext) {
         switch ($ext) {
@@ -145,28 +141,16 @@ class mtfBetter
             break;
         }
     }
-    private function taskManager($add, $_p, $_extension) {
-        $_data = $this->data();
-        $_task_num = $_data['task_num'];
-        $_task_num_time = $_data['task_num_time'];
-        if (time() - $_task_num_time > 3600) {// 超过1分钟，忽略最大任务数，重置当前任务数
-            $_task_num = 0;
-        }
+    private function taskManager($add, $_p = '') {
+        $CONF = $this->CONF;
+        $_task_num = !empty($_SESSION['mtfBetter_task_num']) ? $_SESSION['mtfBetter_task_num'] : 0;
         if ($_task_num > $CONF['arv']['task_num']) {
-            contentType($_extension);
-            die($file_get_contents($_p));
+            if ($_p) $this->outPut($_p);
         } else {
-            if ($_task_num) {
-                $this->data(array(
-                    'task_num' => $add ? ++$_task_num : --$_task_num,
-                    'task_num_time' => time()
-                ));
-            } else {
-                $this->data(array(
-                    'task_num' => $add ? 1 : 0,
-                    'task_num_time' => time()
-                ));
-            }
+            $_SESSION['mtfBetter_task_num'] = $add ? ++$_task_num : --$_task_num;
+        }
+        if ($_task_num < 0) {
+            unset($_SESSION['mtfBetter_task_num']);
         }
     }
 }
--- mtfBetter.php
+++ mtfBetter.php
@@ -1,9 +1,11 @@
 <?php
     ini_set("display_errors", "On");
     error_reporting(E_ALL);
-    include 'mtfBetter.class.php';
-    $mtfBetter = new mtfBetter();
 
-    parse_str($_SERVER['QUERY_STRING'], $arv);
-    $mtfBetter->handler($_arv);
+    if (!empty($_SERVER['QUERY_STRING'])) {
+        parse_str($_SERVER['QUERY_STRING'], $_conf);
+        include 'mtfBetter.class.php';
+        $mtfBetter = new mtfBetter($_conf);
+        $mtfBetter->handler();
+    }
 ?>
