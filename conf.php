4498861138be0604afb8db33e9595fe78c56648f
馒头饭 <mhjlw@126.com>
2020-12-20 22:05:13 +0800

    :new:更改默认配置文件，去掉无用参数

--- conf.php
+++ conf.php
@@ -3,7 +3,6 @@ return array(
     'cache_dir' => dirname(__FILE__) . '/cache/',
     'cache_time' => 60 * 60 * 6, // 缓存时间，秒
     'task_num' => 3, // 最大任务数
-    'available_static' => 0, // 静态资源加速
     'available_pic' => 0, // 图片加速
     'watermark_path' => '', // 水印路径
     'watermark_pos' => '', // 水印位置
